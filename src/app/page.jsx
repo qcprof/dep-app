c16e9874d58a14585f138cc507fdf6c265b78cc5
aaq <152490665+isramasjid@users.noreply.github.com>
2025-02-10 20:01:49 -0500

    Initial Commit

---
+++ src/app/page.jsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <>
+      <h1>Depreciation Calculator</h1>
+      <p>This site will help you calculate depreciation expense using the following methods.</p>
+      <ol>
+        <li>Straight Line Depreciation</li>
+        <li>Sum of the Years Method Depreciation</li>
+        <li>Double Decling Balance Depreciation</li>
+        <li>Units of Production Depreciation</li>
+      </ol>
+
+    </>    
+  )
+}
