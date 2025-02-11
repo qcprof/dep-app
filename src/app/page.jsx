eb36ebf7d0732694b1de7804c677be6c0c8e2a36
aaq <152490665+isramasjid@users.noreply.github.com>
2025-02-10 20:48:23 -0500

    Modified Styling

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,21 +1,25 @@
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl font-bold text-center text-blue-600 mb-4">
+      <h1 className="text-5xl font-bold text-center text-red-700 mt-10">
         Depreciation Calculator
       </h1>
-      <p className="text-lg text-gray-700 text-center mb-6">
+      <p className="text-lg text-black text-left mt-5 ml-10">
         This site will help you calculate depreciation expense using the
         following methods.
       </p>
-      <ol className="list-decimal list-inside bg-gray-100 p-6 rounded-lg shadow-md max-w-md mx-auto">
-        <li className="text-gray-800 mb-2">Straight Line Depreciation</li>
-        <li className="text-gray-800 mb-2">Sum of the Years Method Depreciation</li>
-        <li className="text-gray-800 mb-2">
+      <ol className="list-decimal list-inside bg-zinc-700 p-5  rounded-lg max-w-md ml-10">
+        <li className=" text-zinc-200 mb-1">Straight Line Depreciation</li>
+        <li className=" text-zinc-200 mb-1">
+          Sum of the Years Method Depreciation
+        </li>
+        <li className=" text-zinc-200 mb-1">
           Double Declining Balance Depreciation
         </li>
-        <li className="text-gray-800">Units of Production Depreciation</li>
+        <li className=" text-zinc-200">Units of Production Depreciation</li>
       </ol>
     </>
   );
 }
+
+// font-bold text-center text-blue-600 mb-4
